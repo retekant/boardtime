bba50ec44dc1bbf969498774f718589590cd9f85
retekant <retekant@gmail.com>
2025-07-27 09:13:48 -0700

    basic kv db implemented

--- routes/api/websockets.ts
+++ routes/api/websockets.ts
@@ -1,16 +1,39 @@
 
 const sockets = new Set<WebSocket>();
 
+const kv = await Deno.openKv();
+
+
+
 export function handler(req: Request): Response {
 
-    const{socket, response} = Deno.upgradeWebSocket(req);
+    const { socket, response } = Deno.upgradeWebSocket(req);
 
-    socket.onopen = () => sockets.add(socket);
+    socket.onopen = async () => {
+        sockets.add(socket);
+
+        const drawings = kv.list({ prefix: ["drawings"] });
+
+        for await (const entry of drawings) {
+            if (socket.readyState == WebSocket.OPEN) {
+                socket.send(JSON.stringify(entry.value));
+            }
+        }
+    }
+
     socket.onclose = () => sockets.delete(socket);
 
-    socket.onmessage = (e) => {
-        for(const socketC of sockets) {
-            if(socketC != socket && socketC.readyState == WebSocket.OPEN){
+    socket.onmessage = async (e) => {
+        const data = JSON.parse(e.data);
+
+        if (data.type == "start" || data.type == "draw") {
+            const time = Date.now();
+            await kv.set(["drawings", time], data);
+        }
+
+
+        for (const socketC of sockets) {
+            if (socketC != socket && socketC.readyState == WebSocket.OPEN) {
                 socketC.send(e.data);
             }
         }
